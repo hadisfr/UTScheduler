d58d0390cc2cea52c5944622fde32b057e4d15d2
h@di <hadi@hadisafari.ir>
2018-12-26 20:22:37 +0330

    use decorator to check permissions

--- src/polls/views.py
+++ src/polls/views.py
@@ -7,6 +7,48 @@ from .mail.mail import Mail
 from .mail.notifier import Notifier
 
 
+def needs_involvement(func):
+    def involvement_checked_func(req, poll_id, *args, **kwargs):
+        try:
+            poll = Poll.objects.get(id=poll_id)
+        except ObjectDoesNotExist:
+            raise Http404
+        if poll and poll.audience.filter(name=req.puser.name).exists():
+            return func(req, poll, *args, **kwargs)
+        else:
+            raise Http404
+
+    return involvement_checked_func
+
+
+def needs_ownership(func):
+    def ownership_checked_func(req, poll_id, *args, **kwargs):
+        try:
+            poll = Poll.objects.get(id=poll_id)
+        except ObjectDoesNotExist:
+            raise Http404
+        if poll and poll.owner == req.puser:
+            return func(req, poll, *args, **kwargs)
+        else:
+            raise Http404
+
+    return ownership_checked_func
+
+
+def is_poll_closed(poll):
+    return poll.close_date and poll.close_date < now()
+
+
+def only_open_polls(func):
+    def opennes_checkec_function(req, poll, *args, **kwargs):
+        if not is_poll_closed:
+            raise Http404
+        else:
+            return func(req, poll, *args, **kwargs)
+
+    return opennes_checkec_function
+
+
 def landing(req):
     return redirect(reverse('poll:all'))
 
@@ -52,145 +94,120 @@ def new_poll(req):
         raise Http404
 
 
-def is_poll_closed(poll):
-    return poll.close_date and poll.close_date < now()
+@needs_ownership
+def handle_poll_for_owner(req, poll):
+    return render(req, "polls/poll_details.html", {
+        "poll": poll,
+        "choices": [{"choice": choice, "votes": [
+            {"text": opt[1], "num": Vote.objects.filter(choice=choice, vote=opt[0]).count()}
+            for opt in Vote.VOTE_T
+        ]} for choice in Choice.objects.filter(poll=poll)],
+        "involved_users": User.objects.filter(poll=poll),
+        "users": User.objects.exclude(poll=poll).exclude(owner=poll),
+        "closed": is_poll_closed(poll),
+        "chosen": poll.chosen_choice.choice_text if is_poll_closed(poll) else ""
+    })
 
 
-def handle_poll(req, poll_id):
+@needs_involvement
+def handle_poll_for_audience(req, poll):
     def get_first_vote(l):
         return l[0].get_vote_display() if len(l) else None
 
+    return render(req, "polls/poll_vote.html", {
+        "poll": poll,
+        "choices": [{'choice': choice, 'vote': get_first_vote(Vote.objects.filter(voter=req.puser, choice=choice))}
+                    for choice in Choice.objects.filter(poll=poll)],
+        "possible_votes": [vote[1] for vote in Vote.VOTE_T],
+        "closed": is_poll_closed(poll),
+        "chosen": poll.chosen_choice.choice_text if is_poll_closed(poll) else ""
+    })
+
+
+def handle_poll(req, poll_id):
     try:
-        poll = Poll.objects.get(id=poll_id)
-        user = req.puser
-        choices = Choice.objects.filter(poll=poll)
-        chosen_text = poll.chosen_choice.choice_text if is_poll_closed(poll) else ""
-        if poll.owner == user:
-            return render(req, "polls/poll_details.html", {
-                "poll": poll,
-                "choices": [{"choice": choice, "votes": [
-                    {"text": opt[1], "num": Vote.objects.filter(choice=choice, vote=opt[0]).count()}
-                    for opt in Vote.VOTE_T
-                ]} for choice in choices],
-                "involved_users": User.objects.filter(poll=poll),
-                "users": User.objects.exclude(poll=poll).exclude(owner=poll),
-                "closed": is_poll_closed(poll),
-                "chosen": chosen_text
-            })
-        elif poll.audience.filter(name=req.session.get('username', None)).exists():
-            return render(req, "polls/poll_vote.html", {
-                "poll": poll,
-                "choices": [{'choice': choice, 'vote': get_first_vote(Vote.objects.filter(voter=user, choice=choice))}
-                            for choice in choices],
-                "possible_votes": [vote[1] for vote in Vote.VOTE_T],
-                "closed": is_poll_closed(poll),
-                "chosen": chosen_text
-            })
-        else:
-            return render(req, "login.html", {
-                "msg": "Unauthorized Access",
-            }, status=401)
-    except ObjectDoesNotExist:
-        raise Http404
+        return handle_poll_for_owner(req, poll_id)
+    except Http404:
+        return handle_poll_for_audience(req, poll_id)
 
 
-def add_choice(req, poll_id):
+@needs_ownership
+@only_open_polls
+def add_choice(req, poll):
     if req.method == 'POST':
-        try:
-            poll = Poll.objects.get(id=poll_id)
-            if poll.owner == req.puser and not is_poll_closed(poll):
-                Choice.objects.create(poll=poll, choice_text=req.POST['text'])
-                req.session['msg'] = "Choice created successfully!"
-                return redirect(reverse('poll:show', kwargs={'poll_id': poll.id}))
-            else:
-                raise Http404
-        except ObjectDoesNotExist:
-            raise Http404
+        Choice.objects.create(poll=poll, choice_text=req.POST['text'])
+        req.session['msg'] = "Choice created successfully!"
+        return redirect(reverse('poll:show', kwargs={'poll_id': poll.id}))
     else:
         raise Http404
 
 
-def add_user_to_poll(req, poll_id):
+@needs_ownership
+@only_open_polls
+def add_user_to_poll(req, poll):
     if req.method == 'POST':
         try:
-            poll = Poll.objects.get(id=poll_id)
-            if poll.owner == req.puser and not is_poll_closed(poll):
-                this_audience = User.objects.get(name=req.POST['username'])
-                poll.audience.add(this_audience)
-                uri = req.build_absolute_uri()
-                uri = uri[:uri.rfind('/')]
-                nn = Notifier(Mail())
-                nn.notify_participate(req.puser, this_audience, uri)
-                req.session['msg'] = "User added successfully!"
-                return redirect(reverse('poll:show', kwargs={'poll_id': poll.id}))
-            else:
-                raise Http404
-        except ObjectDoesNotExist:
+            this_audience = User.objects.get(name=req.POST['username'])
+        except (ObjectDoesNotExist, ValueError):
             raise Http404
+        poll.audience.add(this_audience)
+        uri = req.build_absolute_uri()
+        uri = uri[:uri.rfind('/')]
+        nn = Notifier(Mail())
+        nn.notify_participate(req.puser, this_audience, uri)
+        req.session['msg'] = "User added successfully!"
+        return redirect(reverse('poll:show', kwargs={'poll_id': poll.id}))
     else:
         raise Http404
 
 
-def vote(req, poll_id):
+@needs_involvement
+@only_open_polls
+def vote(req, poll):
     choice_prefix = 'vote_'
     if req.method == 'POST':
-        try:
-            poll = Poll.objects.get(id=poll_id)
-            if poll.audience.filter(name=req.puser.name).exists() and not is_poll_closed(poll):
-                for key in req.POST:
-                    if key.startswith(choice_prefix):
-                        choice = Choice.objects.get(id=key[len(choice_prefix):])
-                        if choice.poll != poll:
-                            raise Http404
-                        Vote.objects.update_or_create(
-                            voter=req.puser,
-                            choice=choice,
-                            defaults={'vote': {key: value for (value, key) in Vote.VOTE_T}[req.POST[key]]},
-                        )
-                req.session['msg'] = "Voted successfully!"
-                return redirect(reverse('poll:show', kwargs={'poll_id': poll.id}))
-            else:
-                raise Http404
-        except (ObjectDoesNotExist, ValueError):
-            raise Http404
-    else:
-        raise Http404
-
-
-def end_poll(req, poll_id):
-    if req.method == 'POST':
-        try:
-            poll = Poll.objects.get(id=poll_id)
-            if poll.owner == req.puser and not is_poll_closed(poll):
+        for key in req.POST:
+            if key.startswith(choice_prefix):
                 try:
-                    poll.chosen_choice = Choice.objects.get(id=req.POST['choice'])
-                except ObjectDoesNotExist:
+                    choice = Choice.objects.get(id=key[len(choice_prefix):])
+                except (ObjectDoesNotExist, ValueError):
                     raise Http404
-                poll.close_date = now()
-                poll.save()
-                req.session['msg'] = "Poll closed successfully!"
-                return redirect(reverse('poll:show', kwargs={'poll_id': poll.id}))
-            else:
-                raise Http404
-        except (ObjectDoesNotExist, ValueError):
-            raise Http404
+                if choice.poll != poll:
+                    raise Http404
+                Vote.objects.update_or_create(
+                    voter=req.puser,
+                    choice=choice,
+                    defaults={'vote': {key: value for (value, key) in Vote.VOTE_T}[req.POST[key]]},
+                )
+        req.session['msg'] = "Voted successfully!"
+        return redirect(reverse('poll:show', kwargs={'poll_id': poll.id}))
     else:
         raise Http404
 
 
-def begin_poll(req, poll_id):
+@needs_ownership
+@only_open_polls
+def end_poll(req, poll):
     if req.method == 'POST':
         try:
-            poll = Poll.objects.get(id=poll_id)
-            if poll.owner == req.puser:
-                poll.chosen_choice = None
-                poll.close_date = None
-                poll.save()
-                req.session['msg'] = "Poll reopened successfully!"
-                return redirect(reverse('poll:show', kwargs={'poll_id': poll.id}))
-            else:
-                raise Http404
-        except ObjectDoesNotExist:
+            poll.chosen_choice = Choice.objects.get(id=req.POST['choice'])
+        except (ObjectDoesNotExist, ValueError):
             raise Http404
+        poll.close_date = now()
+        poll.save()
+        req.session['msg'] = "Poll closed successfully!"
+        return redirect(reverse('poll:show', kwargs={'poll_id': poll.id}))
+    else:
+        raise Http404
+
+
+@needs_ownership
+def begin_poll(req, poll):
+    if req.method == 'POST':
+        poll.chosen_choice = None
+        poll.close_date = None
+        poll.save()
+        req.session['msg'] = "Poll reopened successfully!"
+        return redirect(reverse('poll:show', kwargs={'poll_id': poll.id}))
     else:
         raise Http404

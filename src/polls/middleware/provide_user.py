8d84253c044dea0bfc68dec5e151938f4ca7a383
h@di <hadi@hadisafari.ir>
2019-01-01 14:02:35 +0330

    refactor provide_user middleware

--- src/polls/middleware/provide_user.py
+++ src/polls/middleware/provide_user.py
@@ -6,9 +6,10 @@ from ..models import User
 
 def provide_user(get_response):
     def middleware(request):
-        if request.path_info.startswith(reverse('admin:index')):
-            return get_response(request)
-        if request.path_info in [reverse(url) for url in ['login', 'signup']]:
+        if (
+                not request.path_info.startswith(reverse('admin:index'))
+                and request.path_info in [reverse(url) for url in ['login', 'signup']]
+        ):
             try:
                 User.objects.get(name=request.session['username'])
                 return redirect(reverse('landing'))
@@ -18,11 +19,6 @@ def provide_user(get_response):
                     return redirect(reverse('login'))
                 else:
                     return get_response(request)
-        elif request.path_info not in [reverse(url) for url in ['admin:index']]:
-            try:
-                setattr(request, 'puser', User.objects.get(name=request.session['username']))
-            except (ObjectDoesNotExist, ValueError, KeyError):
-                return redirect(reverse('login'))
         response = get_response(request)
         return response
 
